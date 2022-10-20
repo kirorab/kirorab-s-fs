84961ef253560bc4a2cdf9970e4697f4444f3893
LAPTOP-H4VTPR39\rabit <koimeijikoishi@gmail.com>
2022-10-20 23:32:50 +0800

    1.9

--- part1/unicafe/hw/src/App.js
+++ part1/unicafe/hw/src/App.js
@@ -17,22 +17,36 @@ const Feedback = (props) => (
     {props.name} {props.value} {props.percent}
   </div>
 )
-const Statistics = (props) =>(
-  <div>
-      <Feedback name = {props.good} value = {props.goodValue}/>
-      <Feedback name = {props.neutral} value = {props.neutralValue} />
-      <Feedback name = {props.bad} value = {props.badValue}/>
-      <Feedback name = {props.all} value = {props.allValue}/>
-      <Feedback name = {props.average} value = {props.averageValue}/>
-      <Feedback name = {props.postive} value = {props.postiveValue} percent = {props.percent} />
-  </div>
-)
+
+const Statistics = (props) =>{
+  if (props.isGive === 0) {
+    return(
+      <div>
+        No feedback given
+      </div>
+    )
+  } else {
+    return(
+      <div>
+        <Feedback name = {props.good} value = {props.goodValue}/>
+        <Feedback name = {props.neutral} value = {props.neutralValue} />
+        <Feedback name = {props.bad} value = {props.badValue}/>
+        <Feedback name = {props.all} value = {props.allValue}/>
+        <Feedback name = {props.average} value = {props.averageValue}/>
+        <Feedback name = {props.postive} value = {props.postiveValue} percent = {props.percent} />
+      </div>
+    )
+  }
+
+
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  
 
   const setGoodValue = () => () =>{
       console.log('good')
@@ -56,7 +70,8 @@ const App = () => {
       <Button handleClick = {setNeutralValue()} text = "neutral"/>
       <Button handleClick = {setBadValue()} text = "bad"/>
       <Title title="staistics"/>
-      <Statistics good = "good" goodValue = {good} 
+      <Statistics isGive = {good+bad+neutral}
+      good = "good" goodValue = {good} 
       neutral = "neutral" neutralValue = {neutral} 
       bad = "bad" badValue = {bad}
       all = "all" allValue = {good+bad+neutral}
