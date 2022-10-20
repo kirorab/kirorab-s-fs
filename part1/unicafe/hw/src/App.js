806a342b90b8926dfe15b983efa5e6b55323ad3c
LAPTOP-H4VTPR39\rabit <koimeijikoishi@gmail.com>
2022-10-20 23:00:56 +0800

    1.6

--- part1/unicafe/hw/src/App.js
+++ part1/unicafe/hw/src/App.js
@@ -1,16 +1,61 @@
 import { useState } from 'react'
+
+const Button = ({handleClick,text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Title = ({title}) => (
+  <h1>
+    {title}
+  </h1>
+)
+
+const Feedback = (props) => (
+  <p>
+    {props.name} {props.value}
+  </p>
+)
+const Statistics = (props) =>(
+  <div>
+    <p>
+      <Feedback name = {props.good} value = {props.goodValue}/>
+      <Feedback name = {props.neutral} value = {props.neutralValue} />
+      <Feedback name = {props.bad} value = {props.badValue}/>
+    </p>
+  </div>
+)
+
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setGoodValue = () => () =>{
+      console.log('good')
+      setGood(good + 1)
+    }
+  
+  const setNeutralValue = () => () =>{
+      console.log('neutral')
+      setNeutral(neutral + 1)
+    }
+  
+    const setBadValue = () => () =>{
+      console.log('bad')
+      setNeutral(bad + 1)
+    }
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
-        plus
-      </button>
-      <button onClick={() => setCounter(0)}>
-        zero
-      </button>
+      <Title title="give feedback"/>
+      <Button handleClick = {setGoodValue()} text = "good" />
+      <Button handleClick = {setNeutralValue()} text = "neutral"/>
+      <Button handleClick = {setBadValue()} text = "bad"/>
+      <Title title="staistics"/>
+      <Statistics good = "good" goodValue = {good} neutral = "neutral" neutralValue = {neutral} bad = "bad" badValue = {bad}/> 
     </div>
   )
 }
