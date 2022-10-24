31e1945c73e7d8d5dc73a9fd30cbb57903a164ec
LAPTOP-H4VTPR39\rabit <koimeijikoishi@gmail.com>
2022-10-24 19:55:18 +0800

    pa2.1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,3 +1,30 @@
+const Header = ({head}) => (
+  <h1>
+    {head}
+  </h1>
+)
+
+const Part = (props) => (
+  <div>
+    {props.name} {props.exercises}
+  </div>
+)
+
+const Content = ({content}) =>(
+  <div>
+    <Part name = {content[0].name} exercises = {content[0].exercises}/>
+    <Part name = {content[1].name} exercises = {content[1].exercises}/>
+    <Part name = {content[2].name} exercises = {content[2].exercises}/>
+  </div>
+)
+
+const Course = ({course}) => (
+  <div>
+    <Header head={course.name} />
+    <Content content={course.parts} />
+  </div>
+)
+
 const App = () => {
   const course = {
     id: 1,
