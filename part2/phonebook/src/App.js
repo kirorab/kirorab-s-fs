a51f9193c68f6b7f10fa5e237d32f4bca2578dca
LAPTOP-H4VTPR39\rabit <koimeijikoishi@gmail.com>
2022-10-27 23:18:16 +0800

    pa2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,51 @@
 import { useState } from 'react'
 
+
+const Persons = ({person}) => (
+  <div key={person.name}>
+    {person.name}
+  </div>
+)
+
+
+
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ])
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) =>{
+  //console.log(event.target.value);
+  setNewName(event.target.value);
+}
+
+  const addName = (event) => {
+  event.preventDefault()
+  const personObj ={
+    name:newName,
+    key:newName,
+    date:new Date().toISOString,
+    id:persons.length + 1
+  }
+  setPersons(persons.concat(personObj))
+  setNewName('')
+}
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person => <div key={person.name}>{person.name}</div> )}
     </div>
   )
 }
