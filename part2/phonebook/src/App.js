95a3e3e781be118837f8b19a026bbd82903e737a
LAPTOP-H4VTPR39\rabit <koimeijikoishi@gmail.com>
2022-10-29 23:49:58 +0800

    pa2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,22 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import PersonForm from './components/PersonForm';
 import Filter from './components/Filter';
 import Persons from './components/Persons';
-
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNum,setNewNum] = useState('')
   const [newSearch,setNewSearch] = useState('')
   const [showAll,setShowAll] = useState(true)
 
   const notesToShow = showAll ? persons : persons.filter(note => note.name.toLowerCase().includes(newSearch.toLowerCase()))
+
+  useEffect(()=>(axios
+    .get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data)))
+    ,[])
+
 
 const handleSearchChange = (event) =>{
   setNewSearch(event.target.value)
